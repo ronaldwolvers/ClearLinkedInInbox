526341d6c1426af966da6ccbca8267969f2b36da
reaunald <ahcwolvers@gmail.com>
2017-12-17 19:43:05 +0100

    Added some explanatory comments.

--- LinkedIn delete all messages JS snippet.js
+++ LinkedIn delete all messages JS snippet.js
@@ -40,23 +40,30 @@ function deleteFirstVisibleMessage() {
 }
 deleteFirstVisibleMessage();
 
-//Chain promises.
+//Same as above but with chained promises.
+
+//Click the first visible message.
 function clickFirstMessage() {
 	$(".msg-conversation-listitem__link:eq(0)").click();
 }
 
+//Click the "overflow" button (ellipsis / three dots).
 function clickOverFlowButton() {
 	$(".msg-thread__topcard-overflow-icon").click();
 }
 
+//Click the "delete conversation" button.
 function clickDeleteConversationButton() {
 	$(".msg-topcard__full-width-action-button.delete").click();
 }
 
+//Click the "confirm" button.
 function clickDeleteConfirmButton() {
 	$(".msg-modal__button.confirm-delete-btn").click();
 }
 
+
+//Generic function that can execute arbitrary functions after a specified timeout.
 function executeAfterTimeout(functionToExecute, timeout) {
 	return new Promise(resolve => {
 		setTimeout(() => {
@@ -66,4 +73,7 @@ function executeAfterTimeout(functionToExecute, timeout) {
 	});
 }
 
-executeAfterTimeout(clickFirstMessage, 5000).then(executeAfterTimeout.bind(this, clickOverFlowButton, 5000)).then(executeAfterTimeout.bind(this, clickDeleteConversationButton, 5000)).then(executeAfterTimeout.bind(this, clickDeleteConfirmButton, 5000));
+//Keep looping while there are still messages.
+while	($(".msg-conversation-listitem__link:eq(0)").length > 0) {
+	await executeAfterTimeout(clickFirstMessage, 5000).then(executeAfterTimeout.bind(this, clickOverFlowButton, 5000)).then(executeAfterTimeout.bind(this, clickDeleteConversationButton, 5000)).then(executeAfterTimeout.bind(this, clickDeleteConfirmButton, 5000));
+}
