c5b3dee480e5281db914e18d432659b80f734c24
Littlejd1997 <jondavid@schober.org>
2019-01-21 19:42:24 -0600

    Updated because LinkedIn changed

--- LinkedIn delete all messages JS snippet.js
+++ LinkedIn delete all messages JS snippet.js
@@ -1,7 +1,7 @@
 //Execute the Javascript snippet below directly on your LinkedIn messages page to clear your
 //inbox, slowly but steadily.
-//Authored 17 december 2017, http://www.linkedin.com/messaging/
-
+//Authored 21 Jan 2019, http://www.linkedin.com/messaging/
+//Edited by Jonathan Schober (Littlejd1997)
 //https://github.com/ronaldw/ClearLinkedInInbox
 
 //Click the first visible message.
@@ -10,18 +10,19 @@ function clickFirstMessage() {
 }
 
 //Click the "overflow" button (ellipsis / three dots).
+//No longer Needed
 function clickOverFlowButton() {
-	$(".msg-thread__topcard-overflow-icon").click();
+	$(".msg-thread-actions__dropdown").children().first().click();
 }
 
 //Click the "delete conversation" button.
 function clickDeleteConversationButton() {
-	$(".msg-topcard__full-width-action-button.delete").click();
+	$('#ember110').click()
 }
 
 //Click the "confirm" button.
 function clickDeleteConfirmButton() {
-	$(".msg-modal__button.confirm-delete-btn").click();
+	$(".js-msg-delete").click()
 }
 
 
@@ -38,9 +39,9 @@ function executeAfterTimeout(functionToExecute, timeout) {
 
 //Timeout (in milliseconds) in between every step. Due to asynchronous calls being made on the page
 //a timeout is necessary, but the value of the timeout was arbitrarily chosen.
-const timeout = 1000;
+const timeout = 500;
 
 //Keep looping while there are still messages.
 while	($(".msg-conversation-listitem__link:eq(0)").length > 0) {
-	await executeAfterTimeout(clickFirstMessage, timeout).then(executeAfterTimeout.bind(this, clickOverFlowButton, timeout)).then(executeAfterTimeout.bind(this, clickDeleteConversationButton, timeout)).then(executeAfterTimeout.bind(this, clickDeleteConfirmButton, timeout));
+	await executeAfterTimeout(clickFirstMessage, timeout). then(executeAfterTimeout.bind(this, clickDeleteConversationButton, timeout)).then(executeAfterTimeout.bind(this, clickDeleteConfirmButton, timeout));
 }
